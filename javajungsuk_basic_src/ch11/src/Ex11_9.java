fc954f90f36e4e2deab7ceac20ac0bb77a0e3834
SEONG NAMKUNG <castello@naver.com>
2020-07-28 07:53:58 +0900

    Update Ex11_9.java

--- javajungsuk_basic_src/ch11/src/Ex11_9.java
+++ javajungsuk_basic_src/ch11/src/Ex11_9.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 class Ex11_9 {
 	public static void main(String[] args) {
+		Object[] objArr = {"1",new Integer(1),"2","2","3","3","4","4","4"};
 		Set set = new HashSet();
-		
-		for (int i = 0; set.size() < 6 ; i++) {
-			int num = (int)(Math.random()*45) + 1;
-			set.add(new Integer(num));
-		}
 
-		List list = new LinkedList(set); // LinkedList(Collection c)
-		Collections.sort(list);          // Collections.sort(List list)
-		System.out.println(list);
+		for(int i=0; i < objArr.length; i++) {
+			set.add(objArr[i]);	// HashSet에 objArr의 요소들을 저장한다.
+		}
+        // HashSet에 저장된 요소들을 출력한다.
+		System.out.println(set);	
+
+		// HashSet에 저장된 요소들을 출력한다.(Iterator이용)
+		Iterator it = set.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());	
+		}
 	}
 }
