5f8238e91dd1721c5c8f9523caf9aeb9b6582b0c
SEONG NAMKUNG <castello@naver.com>
2020-07-28 07:54:18 +0900

    Update Ex11_9.java

--- javajungsuk_basic_src/ch11/src/Ex11_9.java
+++ javajungsuk_basic_src/ch11/src/Ex11_9.java
@@ -8,7 +8,7 @@ class Ex11_9 {
 		for(int i=0; i < objArr.length; i++) {
 			set.add(objArr[i]);	// HashSet에 objArr의 요소들을 저장한다.
 		}
-        // HashSet에 저장된 요소들을 출력한다.
+                // HashSet에 저장된 요소들을 출력한다.
 		System.out.println(set);	
 
 		// HashSet에 저장된 요소들을 출력한다.(Iterator이용)
