36f22367213b584439c9906a0f3d5ee6cd376336
SEONG NAMKUNG <castello@naver.com>
2020-07-28 07:55:25 +0900

    Update Ex11_10.java

--- javajungsuk_basic_src/ch11/src/Ex11_10.java
+++ javajungsuk_basic_src/ch11/src/Ex11_10.java
@@ -1,23 +1,16 @@
-import java.util.*; 
+import java.util.*;
 
-class Ex11_10 { 
-	public static void main(String[] args) { 
-		Set set = new HashSet(); 
-//    Set set = new LinkedHashSet(); 
-		int[][] board = new int[5][5]; 
+class Ex11_10 {
+	public static void main(String[] args) {
+		Set set = new HashSet();
+		
+		for (int i = 0; set.size() < 6 ; i++) {
+			int num = (int)(Math.random()*45) + 1;
+			set.add(new Integer(num));
+		}
 
-		for(int i=0; set.size() < 25; i++) { 
-			set.add((int)(Math.random()*50)+1+""); 
-		} 
-
-		Iterator it = set.iterator(); 
-
-		for(int i=0; i < board.length; i++) { 
-			for(int j=0; j < board[i].length; j++) { 
-				board[i][j] = Integer.parseInt((String)it.next()); 
-				System.out.print((board[i][j] < 10 ? "  " : " ") + board[i][j]); 
-			} 
-			System.out.println(); 
-		} 
-	} // main
+		List list = new LinkedList(set); // LinkedList(Collection c)
+		Collections.sort(list);          // Collections.sort(List list)
+		System.out.println(list);
+	}
 }
