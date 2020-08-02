59b55a36be42c71980bf97177f42cc429958ae98
SEONG NAMKUNG <castello@naver.com>
2020-08-03 00:46:58 +0900

    Update Ex11_18.java

--- javajungsuk_basic_src/ch11/src/Ex11_18.java
+++ javajungsuk_basic_src/ch11/src/Ex11_18.java
@@ -9,9 +9,9 @@ class Ex11_18 {
 		for(int i=0; i < data.length; i++) {
 			if(map.containsKey(data[i])) {
 				Integer value = (Integer)map.get(data[i]);
-				map.put(data[i], new Integer(value.intValue() + 1));
+				map.put(data[i], value + 1);
 			} else {
-				map.put(data[i], new Integer(1));			
+				map.put(data[i], 1);			
 			}
 		}
 
